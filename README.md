98d958b45cda1c21c8cddb51109c891380a07903
YFROBOTZL <yfrobot-zl@foxmail.com>
2022-01-08 09:45:56 +0800

    V0.0.1

---
+++ README.md
@@ -0,0 +1,13 @@
+# YFROBOT颜色传感器
+ 颜色传感器模块 microbit makecode 扩展
+
+
+## License
+
+MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
---
+++ main.ts
@@ -0,0 +1,250 @@
+/** 
+ * @file TCS34725 颜色传感器
+ * @brief YFROBOT's sensors makecode library.
+ * @n This is a MakeCode graphics programming extension for YFROBOT's sensors module.
+ * 
+ * @copyright    YFROBOT,2022
+ * @copyright    MIT Lesser General Public License
+ * 
+ * @author [email](yfrobot@qq.com)
+ * @date  2022-01-08
+*/
+
+enum RGB {
+    //% block="红"
+    RED,
+    //% block="绿"
+    GREEN,
+    //% block="蓝"
+    BLUE,
+    //% block="全部"
+    CLEAR
+}
+//% weight=50 color=#45b787 icon="\uf12e" block="颜色传感器"
+namespace TCS34725_SENSOR {
+    enum LCS_Constants {
+        // Constants
+        ADDRESS = 0x29,
+        ID = 0x12, // Register should be equal to 0x44 for the TCS34721 or TCS34725, or 0x4D for the TCS34723 or TCS34727.
+
+        COMMAND_BIT = 0x80,
+
+        ENABLE = 0x00,
+        ENABLE_AIEN = 0x10, // RGBC Interrupt Enable
+        ENABLE_WEN = 0x08, // Wait enable - Writing 1 activates the wait timer
+        ENABLE_AEN = 0x02, // RGBC Enable - Writing 1 actives the ADC, 0 disables it
+        ENABLE_PON = 0x01, // Power on - Writing 1 activates the internal oscillator, 0 disables it
+        ATIME = 0x01, // Integration time
+        WTIME = 0x03, // Wait time (if ENABLE_WEN is asserted)
+        AILTL = 0x04, // Clear channel lower interrupt threshold
+        AILTH = 0x05,
+        AIHTL = 0x06, // Clear channel upper interrupt threshold
+        AIHTH = 0x07,
+        PERS = 0x0C, // Persistence register - basic SW filtering mechanism for interrupts
+        PERS_NONE = 0x00, // Every RGBC cycle generates an interrupt
+        PERS_1_CYCLE = 0x01, // 1 clean channel value outside threshold range generates an interrupt
+        PERS_2_CYCLE = 0x02, // 2 clean channel values outside threshold range generates an interrupt
+        PERS_3_CYCLE = 0x03, // 3 clean channel values outside threshold range generates an interrupt
+        PERS_5_CYCLE = 0x04, // 5 clean channel values outside threshold range generates an interrupt
+        PERS_10_CYCLE = 0x05, // 10 clean channel values outside threshold range generates an interrupt
+        PERS_15_CYCLE = 0x06, // 15 clean channel values outside threshold range generates an interrupt
+        PERS_20_CYCLE = 0x07, // 20 clean channel values outside threshold range generates an interrupt
+        PERS_25_CYCLE = 0x08, // 25 clean channel values outside threshold range generates an interrupt
+        PERS_30_CYCLE = 0x09, // 30 clean channel values outside threshold range generates an interrupt
+        PERS_35_CYCLE = 0x0A, // 35 clean channel values outside threshold range generates an interrupt
+        PERS_40_CYCLE = 0x0B, // 40 clean channel values outside threshold range generates an interrupt
+        PERS_45_CYCLE = 0x0C, // 45 clean channel values outside threshold range generates an interrupt
+        PERS_50_CYCLE = 0x0D, // 50 clean channel values outside threshold range generates an interrupt
+        PERS_55_CYCLE = 0x0E, // 55 clean channel values outside threshold range generates an interrupt
+        PERS_60_CYCLE = 0x0F, // 60 clean channel values outside threshold range generates an interrupt
+        CONFIG = 0x0D,
+        CONFIG_WLONG = 0x02, // Choose between short and long (12x) wait times via WTIME
+        CONTROL = 0x0F, // Set the gain level for the sensor
+        STATUS = 0x13,
+        STATUS_AINT = 0x10, // RGBC Clean channel interrupt
+        STATUS_AVALID = 0x01, // Indicates that the RGBC channels have completed an integration cycle
+
+        CDATAL = 0x14, // Clear channel data
+        CDATAH = 0x15,
+        RDATAL = 0x16, // Red channel data
+        RDATAH = 0x17,
+        GDATAL = 0x18, // Green channel data
+        GDATAH = 0x19,
+        BDATAL = 0x1A, // Blue channel data
+        BDATAH = 0x1B,
+
+        GAIN_1X = 0x00, //  1x gain
+        GAIN_4X = 0x01, //  4x gain
+        GAIN_16X = 0x02, // 16x gain
+        GAIN_60X = 0x03  // 60x gain
+    }
+
+    let LCS_integration_time_val = 0
+
+    // I2C functions
+
+    function I2C_WriteReg8(addr: number, reg: number, val: number) {
+        let buf = pins.createBuffer(2)
+        buf.setNumber(NumberFormat.UInt8BE, 0, reg)
+        buf.setNumber(NumberFormat.UInt8BE, 1, val)
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function I2C_ReadReg8(addr: number, reg: number): number {
+        let buf = pins.createBuffer(1)
+        buf.setNumber(NumberFormat.UInt8BE, 0, reg)
+        pins.i2cWriteBuffer(addr, buf)
+        buf = pins.i2cReadBuffer(addr, 1)
+        return buf.getNumber(NumberFormat.UInt8BE, 0);
+    }
+
+    function I2C_ReadReg16(addr: number, reg: number): number {
+        let buf = pins.createBuffer(1)
+        buf.setNumber(NumberFormat.UInt8BE, 0, reg)
+        pins.i2cWriteBuffer(addr, buf)
+        buf = pins.i2cReadBuffer(addr, 2)
+        // Little endian
+        return ((buf.getNumber(NumberFormat.UInt8BE, 1) << 8) | buf.getNumber(NumberFormat.UInt8BE, 0));
+    }
+
+    //% blockId="initialize_sensor" block="初始化颜色传感器"
+    export function LCS_initialize() {
+        // Make sure we're connected to the right sensor.
+        let chip_id = I2C_ReadReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ID))
+
+        if (chip_id != 0x44) {
+            return // Incorrect chip ID
+        }
+
+        // Set default integration time and gain.
+        LCS_set_integration_time(0.0048)
+        LCS_set_gain(LCS_Constants.GAIN_16X)
+
+        // Enable the device (by default, the device is in power down mode on bootup).
+        LCS_enable()
+    }
+
+    function LCS_enable() {
+        // Set the power and enable bits.
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ENABLE), LCS_Constants.ENABLE_PON)
+        basic.pause(10) // not sure if this is right    time.sleep(0.01) // FIXME delay for 10ms
+
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ENABLE), (LCS_Constants.ENABLE_PON | LCS_Constants.ENABLE_AEN))
+    }
+
+    function LCS_set_integration_time(time: number) {
+        let val = 0x100 - (time / 0.0024) // FIXME was cast to int type
+        if (val > 255) {
+            val = 255
+        } else if (val < 0) {
+            val = 0
+        }
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ATIME), val)
+        LCS_integration_time_val = val
+    }
+
+    function LCS_set_gain(gain: number) {
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.CONTROL), gain)
+    }
+
+
+    function LCS_set_led_state(state: boolean) {
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.PERS), LCS_Constants.PERS_NONE)
+        let val = I2C_ReadReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ENABLE))
+        if (state) {
+            val |= LCS_Constants.ENABLE_AIEN
+        } else {
+            val &= ~LCS_Constants.ENABLE_AIEN
+        }
+        I2C_WriteReg8(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.ENABLE), val)
+
+        basic.pause(2 * (256 - LCS_integration_time_val) * 2.4) // delay for long enough for there to be new (post-change) complete values available
+    }
+
+    //% blockId="getColor" block="读取颜色的颜色是"
+    export function getColor(): RGB {
+        basic.pause((256 - LCS_integration_time_val) * 2.4);
+        
+         let r = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.RDATAL));
+         let g =  I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.GDATAL));
+         let b = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.BDATAL));
+         
+         serial.writeLine("R:"+r + " G:" + g + " B:" + b);
+         
+         let color = RGB.RED;
+         let max = r;
+         if(g > max){
+             max = g;
+             color = RGB.GREEN;
+         }
+         if(b > max){
+             max = b;
+             color = RGB.BLUE;
+         }
+
+        serial.writeLine("val: " + color);
+        return color;
+    }
+    //% blockId="colorType" block="颜色值 %colorType"
+    export function colorType(colorType:RGB): RGB{
+        return colorType;
+    }
+
+    //% blockId="getSensorData" block="读取颜色值 %colorId"
+    export function getColorData(color: RGB): number {
+        basic.pause((256 - LCS_integration_time_val) * 2.4);
+        let sum = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.CDATAL));
+        let vue = 0;
+        switch (color) {
+            case RGB.RED:
+                vue = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.RDATAL));
+
+                break;
+            case RGB.GREEN:
+                vue = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.GDATAL));
+
+                break;
+            case RGB.BLUE:
+                vue = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.BDATAL));
+
+                break;
+            case RGB.CLEAR:
+                return sum;
+                break;
+
+        }
+        vue = Math.floor(vue / sum * 255);
+
+        serial.writeLine("val: " + vue);
+        return vue;
+    }
+
+
+    function LCS_get_raw_data(delay: boolean = false): number[] {
+        if (delay) {
+            // Delay for the integration time to allow reading immediately after the previous read.
+            basic.pause((256 - LCS_integration_time_val) * 2.4)
+        }
+
+        let div = (256 - LCS_integration_time_val) * 1024
+        let rgbc = [0, 0, 0, 0]
+        rgbc[0] = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.RDATAL)) / div
+        rgbc[1] = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.GDATAL)) / div
+        rgbc[2] = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.BDATAL)) / div
+        rgbc[3] = I2C_ReadReg16(LCS_Constants.ADDRESS, (LCS_Constants.COMMAND_BIT | LCS_Constants.CDATAL)) / div
+        if (rgbc[0] > 1) {
+            rgbc[0] = 1
+        }
+        if (rgbc[1] > 1) {
+            rgbc[1] = 1
+        }
+        if (rgbc[2] > 1) {
+            rgbc[2] = 1
+        }
+        if (rgbc[3] > 1) {
+            rgbc[3] = 1
+        }
+        return rgbc
+    }
+}
+ 
